2b4d9bf3e9a8fa1e6769ff01c85965569064890e
Anargyros Tsadimas <tsadimas@hua.gr>
2025-05-14 15:52:10 +0300

    Update startup_script.sh

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-echo "✅ Kubectl and Helm installed successfully"
+
 
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
@@ -8,20 +8,5 @@ brew install derailed/k9s/k9s
 
 echo "✅ kubectx, kubens, fzf, and k9s installed successfully"
 sudo apt-get install fzf -y
-
-echo 'alias k="kubectl"' >> /home/$USER/.bashrc
-echo 'alias kga="kubectl get all"' >> /home/$USER/.bashrc
-echo 'alias kgn="kubectl get all --all-namespaces"' >> /home/$USER/.bashrc
-echo 'alias kdel="kubectl delete"' >> /home/$USER/.bashrc
-echo 'alias kd="kubectl describe"' >> /home/$USER/.bashrc
-echo 'alias kg="kubectl get"' >> /home/$USER/.bashrc
-
-echo "✅ The following aliases were added:"
-echo "alias k=kubectl" >> /home/$USER/.bashrc
-echo "alias kga=kubectl get all" >> /home/$USER/.bashrc
-echo "alias kgn=kubectl get all --all-namespaces" >> /home/$USER/.bashrc
-echo "alias kdel=kubectl delete" >> /home/$USER/.bashrc
-echo "alias kd=kubectl describe" >> /home/$USER/.bashrc
-echo "alias kg=kubectl get" >> /home/$USER/.bashrc
 
 source ~/.bashrc 
