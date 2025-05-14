d35065e499c4388e70b569de11fcb4ae2e1c9390
Anargyros Tsadimas <tsadimas@hua.gr>
2025-05-14 16:03:45 +0300

    Update startup_script.sh

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+
+sudo apt-get update
+
+sudo apt-get install -y kubectl
+
+sudo apt-get install -y docker.io
+
 # Install Minikube and dependencies
 echo "Installing Minikube and dependencies..."
 sudo apt-get update && sudo apt-get install -y curl conntrack
@@ -8,7 +15,9 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
 
+
+sudo service docker start
 # Start Minikube using the "none" driver
 echo "Starting Minikube..."
-sudo minikube start
+minikube start –driver=docker
 
